31096c8a3dbdb9b8aeb5fc545f5ad7c4a91b6e82
luigi <luigicussigh59@gmail.com>
2025-04-16 16:52:15 +0900

    cleanup

--- assignment_05/hw05_2023148006/squarePyramid.js
+++ assignment_05/hw05_2023148006/squarePyramid.js
@@ -8,19 +8,18 @@ class squarePyramid
 
 2) Vertex indices
     Vertex indices of the unit squarePyramid is as follows:
-   
          v0
-     
+
       v4-----v3
      /      /
-    v1----v2 
+    v1----v2
 
     The order of faces and their vertex indices is as follows:
-        front (0,1,2,3), right (0,3,4,5), top (0,5,6,1), 
+        front (0,1,2,3), right (0,3,4,5), top (0,5,6,1),
         left (1,6,7,2), bottom (7,4,3,2), back (4,7,6,5)
-    Note that each face has two triangles, 
+    Note that each face has two triangles,
     so the total number of triangles is 12 (6 faces * 2 triangles)
-    And, we need to maintain the order of vertices for each triangle as 
+    And, we need to maintain the order of vertices for each triangle as
     counterclockwise (when we see the face from the outside of the squarePyramid):
         front [(0,1,2), (2,3,0)]
         right [(0,3,4), (4,5,0)]
@@ -32,14 +31,14 @@ class squarePyramid
 3) Vertex normals
     Each vertex in the same face has the same normal vector (flat shading)
     The normal vector is the same as the face normal vector
-    front face: (0,0,1), right face: (1,0,0), top face: (0,1,0), 
-    left face: (-1,0,0), bottom face: (0,-1,0), back face: (0,0,-1) 
+    front face: (0,0,1), right face: (1,0,0), top face: (0,1,0),
+    left face: (-1,0,0), bottom face: (0,-1,0), back face: (0,0,-1)
 
 4) Vertex colors
     Each vertex in the same face has the same color (flat shading)
     The color is the same as the face color
-    front face: red (1,0,0,1), right face: yellow (1,1,0,1), top face: green (0,1,0,1), 
-    left face: cyan (0,1,1,1), bottom face: blue (0,0,1,1), back face: magenta (1,0,1,1) 
+    front face: red (1,0,0,1), right face: yellow (1,1,0,1), top face: green (0,1,0,1),
+    left face: cyan (0,1,1,1), bottom face: blue (0,0,1,1), back face: magenta (1,0,1,1)
 
 5) Vertex texture coordinates
     Each vertex in the same face has the same texture coordinates (flat shading)
@@ -65,7 +64,7 @@ class squarePyramid
 export class squarePyramid {
     constructor(gl, options = {}) {
         this.gl = gl;
-        
+
         // Creating VAO and buffers
         this.vao = gl.createVertexArray();
         this.vbo = gl.createBuffer();
@@ -74,15 +73,15 @@ export class squarePyramid {
         // Initializing data
         this.vertices = new Float32Array([
             // front face  (v0,v1,v2)
-            0.0,  1.0,  0.0, -0.5,  0.0,  0.5,  0.5,  0.0,  0.5,
+            0.0, 1.0, 0.0, -0.5, 0.0, 0.5, 0.5, 0.0, 0.5,
             // right face  (v0,v2,v3)
-            0.0,  1.0,  0.0,  0.5,  0.0,  0.5,  0.5,  0.0, -0.5,
+            0.0, 1.0, 0.0, 0.5, 0.0, 0.5, 0.5, 0.0, -0.5,
             // back face   (v0,v3,v4)
-            0.0,  1.0,  0.0,  0.5,  0.0, -0.5, -0.5,  0.0, -0.5,
+            0.0, 1.0, 0.0, 0.5, 0.0, -0.5, -0.5, 0.0, -0.5,
             // left face   (v0,v4,v1)
-            0.0,  1.0,  0.0, -0.5,  0.0, -0.5, -0.5,  0.0,  0.5,
+            0.0, 1.0, 0.0, -0.5, 0.0, -0.5, -0.5, 0.0, 0.5,
             // bottom face (v1, v2, v3, v4)
-           -0.5,  0.0,  0.5,  0.5,  0.0,  0.5,  0.5,  0.0, -0.5,  -0.5,  0.0, -0.5,
+            -0.5, 0.0, 0.5, 0.5, 0.0, 0.5, 0.5, 0.0, -0.5, -0.5, 0.0, -0.5,
         ]);
 
         // this.normals = new Float32Array([
@@ -104,24 +103,24 @@ export class squarePyramid {
         if (options.color) {
             for (let i = 0; i < 24 * 4; i += 4) {
                 this.colors[i] = options.color[0];
-                this.colors[i+1] = options.color[1];
-                this.colors[i+2] = options.color[2];
-                this.colors[i+3] = options.color[3];
+                this.colors[i + 1] = options.color[1];
+                this.colors[i + 2] = options.color[2];
+                this.colors[i + 3] = options.color[3];
             }
         }
         else {
             this.colors = new Float32Array([
                 // front face (v0,v1,v2,v3) - red
-                1, 0, 0, 1,   1, 0, 0, 1,   1, 0, 0, 1, 
+                1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1,
                 // right face (v0,v3,v4,v5) - yellow
-                1, 1, 0, 1,   1, 1, 0, 1,   1, 1, 0, 1, 
+                1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1,
                 // top face (v0,v5,v6,v1) - green
-                1, 0, 1, 1,   1, 0, 1, 1,   1, 0, 1, 1, 
+                1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1,
                 // left face (v1,v6,v7,v2) - cyan
-                0, 1, 1, 1,   0, 1, 1, 1,   0, 1, 1, 1, 
+                0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1,
                 // bottom face (v7,v4,v3,v2) - blue
-                0, 0, 1, 1,   0, 0, 1, 1,   0, 0, 1, 1, 
-                0, 0, 1, 1,   0, 0, 1, 1,   0, 0, 1, 1, 
+                0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1,
+                0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1,
             ]);
         }
 
@@ -164,18 +163,18 @@ export class squarePyramid {
         // this.faceNormals = new Float32Array(72);
         // this.faceNormals.set(this.normals);
 
-        // compute vertex normals 
+        // compute vertex normals
         // for (let i = 0; i < 24; i += 3) {
 
-        //     let vn_x = (this.normals[this.sameVertices[i]*3] + 
-        //                this.normals[this.sameVertices[i+1]*3] + 
-        //                this.normals[this.sameVertices[i+2]*3]) / 3; 
-        //     let vn_y = (this.normals[this.sameVertices[i]*3 + 1] + 
-        //                this.normals[this.sameVertices[i+1]*3 + 1] + 
-        //                this.normals[this.sameVertices[i+2]*3 + 1]) / 3; 
-        //     let vn_z = (this.normals[this.sameVertices[i]*3 + 2] + 
-        //                this.normals[this.sameVertices[i+1]*3 + 2] + 
-        //                this.normals[this.sameVertices[i+2]*3 + 2]) / 3; 
+        //     let vn_x = (this.normals[this.sameVertices[i]*3] +
+        //                this.normals[this.sameVertices[i+1]*3] +
+        //                this.normals[this.sameVertices[i+2]*3]) / 3;
+        //     let vn_y = (this.normals[this.sameVertices[i]*3 + 1] +
+        //                this.normals[this.sameVertices[i+1]*3 + 1] +
+        //                this.normals[this.sameVertices[i+2]*3 + 1]) / 3;
+        //     let vn_z = (this.normals[this.sameVertices[i]*3 + 2] +
+        //                this.normals[this.sameVertices[i+1]*3 + 2] +
+        //                this.normals[this.sameVertices[i+2]*3 + 2]) / 3;
 
         //     this.vertexNormals[this.sameVertices[i]*3] = vn_x;
         //     this.vertexNormals[this.sameVertices[i+1]*3] = vn_x;
@@ -204,9 +203,9 @@ export class squarePyramid {
 
         // 버퍼 크기 계산
         const vSize = this.vertices.byteLength;
-        const nSize = 0 //this.normals.byteLength;
+        const nSize = 0 // this.normals.byteLength;
         const cSize = this.colors.byteLength;
-        const tSize = 0//this.texCoords.byteLength;
+        const tSize = 0// this.texCoords.byteLength;
         const totalSize = vSize + nSize + cSize + tSize;
 
         gl.bindVertexArray(this.vao);
@@ -215,9 +214,9 @@ export class squarePyramid {
         gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo);
         gl.bufferData(gl.ARRAY_BUFFER, totalSize, gl.STATIC_DRAW);
         gl.bufferSubData(gl.ARRAY_BUFFER, 0, this.vertices);
-       // gl.bufferSubData(gl.ARRAY_BUFFER, vSize, this.normals);
+        // gl.bufferSubData(gl.ARRAY_BUFFER, vSize, this.normals);
         gl.bufferSubData(gl.ARRAY_BUFFER, vSize + nSize, this.colors);
-       // gl.bufferSubData(gl.ARRAY_BUFFER, vSize + nSize + cSize, this.texCoords);
+        // gl.bufferSubData(gl.ARRAY_BUFFER, vSize + nSize + cSize, this.texCoords);
 
         // EBO에 인덱스 데이터 복사
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.ebo);
@@ -227,13 +226,13 @@ export class squarePyramid {
         gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);  // position
         //gl.vertexAttribPointer(1, 3, gl.FLOAT, false, 0, vSize);  // normal
         gl.vertexAttribPointer(2, 4, gl.FLOAT, false, 0, vSize + nSize);  // color
-       // gl.vertexAttribPointer(3, 2, gl.FLOAT, false, 0, vSize + nSize + cSize);  // texCoord
+        // gl.vertexAttribPointer(3, 2, gl.FLOAT, false, 0, vSize + nSize + cSize);  // texCoord
 
         // vertex attributes 활성화
         gl.enableVertexAttribArray(0);
-       // gl.enableVertexAttribArray(1);
+        // gl.enableVertexAttribArray(1);
         gl.enableVertexAttribArray(2);
-       // gl.enableVertexAttribArray(3);
+        // gl.enableVertexAttribArray(3);
 
         // 버퍼 바인딩 해제
         gl.bindBuffer(gl.ARRAY_BUFFER, null);
@@ -246,10 +245,10 @@ export class squarePyramid {
 
         gl.bindVertexArray(this.vao);
         gl.bindBuffer(gl.ARRAY_BUFFER, this.vbo);
-        
+
         // normals 데이터만 업데이트
         // gl.bufferSubData(gl.ARRAY_BUFFER, vSize, this.normals);
-        
+
         gl.bindBuffer(gl.ARRAY_BUFFER, null);
         gl.bindVertexArray(null);
     }
@@ -268,4 +267,4 @@ export class squarePyramid {
         gl.deleteBuffer(this.ebo);
         gl.deleteVertexArray(this.vao);
     }
-} 
+}
