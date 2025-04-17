3cd4107d67e42cee0b98877e7252402d64099017
luigi <luigicussigh59@gmail.com>
2025-04-17 13:41:42 +0900

    hw06 initial setup

---
+++ assignment_06/hw06_2023148006/textured_octhedron.js
@@ -0,0 +1,147 @@
+import { resizeAspectRatio, setupText, updateText, Axes } from '../util/util.js';
+import { Shader, readShaderFile } from '../../util/shader.js';
+import { regularOctahedron } from './regularOctahedron.js';
+import { Arcball } from '../../util/arcball.js';
+
+const canvas = document.getElementById('glCanvas');
+const gl = canvas.getContext('webgl2');
+let shader;
+let textOverlay;
+let isInitialized = false;
+
+let viewMatrix = mat4.create();
+let projMatrix = mat4.create();
+let modelMatrix = mat4.create();
+
+const cube = new Cube(gl);
+const axes = new Axes(gl, 2.2); // create an Axes object with the length of axis 1.5
+
+// Arcball object: initial distance 5.0, rotation sensitivity 2.0, zoom sensitivity 0.0005
+// default of rotation sensitivity = 1.5, default of zoom sensitivity = 0.001
+let initialDistance = 5.0;
+let arcBallMode = 'CAMERA';     // 'CAMERA' or 'MODEL'
+const arcball = new Arcball(canvas, initialDistance, { rotation: 2.0, zoom: 0.0005 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (isInitialized) {
+        console.log("Already initialized");
+        return;
+    }
+
+    main().then(success => {
+        if (!success) {
+            console.log('program terminated');
+            return;
+        }
+        isInitialized = true;
+    }).catch(error => {
+        console.error('program terminated with error:', error);
+    });
+});
+
+function setupKeyboardEvents() {
+    document.addEventListener('keydown', (event) => {
+        if (event.key == 'a') {
+            // console.log("a key pressed");
+            if (arcBallMode == 'CAMERA') {
+                arcBallMode = 'MODEL';
+            }
+            else {
+                arcBallMode = 'CAMERA';
+            }
+            updateText(textOverlay, "arcball mode: " + arcBallMode);
+        }
+        else if (event.key == 'r') {
+            arcball.reset();
+            modelMatrix = mat4.create();
+            arcBallMode = 'CAMERA';
+            updateText(textOverlay, "arcball mode: " + arcBallMode);
+        }
+    });
+}
+
+function initWebGL() {
+    if (!gl) {
+        console.error('WebGL 2 is not supported by your browser.');
+        return false;
+    }
+
+    canvas.width = 700;
+    canvas.height = 700;
+    resizeAspectRatio(gl, canvas);
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(0.1, 0.2, 0.3, 1.0);
+
+    return true;
+}
+
+async function initShader() {
+    const vertexShaderSource = await readShaderFile('shVert.glsl');
+    const fragmentShaderSource = await readShaderFile('shFrag.glsl');
+    shader = new Shader(gl, vertexShaderSource, fragmentShaderSource);
+}
+
+function render() {
+
+    // clear canvas
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.enable(gl.DEPTH_TEST);
+
+    if (arcBallMode == 'CAMERA') {
+        viewMatrix = arcball.getViewMatrix();
+    }
+    else { // arcBallMode == 'MODEL'
+        modelMatrix = arcball.getModelRotMatrix();
+        viewMatrix = arcball.getViewCamDistanceMatrix();
+    }
+
+    // drawing the cube
+    shader.use();  // using the cube's shader
+    shader.setMat4('u_model', modelMatrix);
+    shader.setMat4('u_view', viewMatrix);
+    shader.setMat4('u_projection', projMatrix);
+    cube.draw(shader);
+
+    // drawing the axes (using the axes's shader: see util.js)
+    axes.draw(viewMatrix, projMatrix);
+
+    // call the render function the next time for animation
+    requestAnimationFrame(render);
+}
+
+async function main() {
+    try {
+        if (!initWebGL()) {
+            throw new Error('WebGL initialization failed');
+        }
+
+        await initShader();
+
+        // Initial view transformation matrix (camera at (0,0,-initialDistance)
+        mat4.translate(viewMatrix, viewMatrix, vec3.fromValues(0, 0, -initialDistance));
+
+        // Projection transformation matrix (invariant in the program)
+        mat4.perspective(
+            projMatrix,
+            glMatrix.toRadian(60),  // field of view (fov, degree)
+            canvas.width / canvas.height, // aspect ratio
+            0.1, // near
+            100.0 // far
+        );
+
+        textOverlay = setupText(canvas, "arcball mode: " + arcBallMode, 1);
+        setupText(canvas, "press 'a' to change arcball mode", 2);
+        setupText(canvas, "press 'r' to reset arcball", 3);
+        setupKeyboardEvents();
+
+        // call the render function the first time for animation
+        requestAnimationFrame(render);
+
+        return true;
+
+    } catch (error) {
+        console.error('Failed to initialize program:', error);
+        alert('Failed to initialize program');
+        return false;
+    }
+}
